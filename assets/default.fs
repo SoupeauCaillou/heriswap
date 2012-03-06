5c3cdcb13f9cc66a6fdf5bce1bcfa0ba9447c9a3
Pierre-Eric Pelloux-Prayer <pelloux@gmail.com>
2012-03-06 20:16:08 +0100

    Revert "Support texture less rendering"
    This reverts commit 0cedff8e6f32320c5967d84ab74c38cfdd374ef9.

--- assets/default.fs
+++ assets/default.fs
@@ -2,15 +2,12 @@
 precision mediump float;
 #endif
 uniform sampler2D tex0;
-uniform int useTexturing;
 
 varying vec2 uvVarying;
 varying vec4 vColor;
 
 void main()
 {
-	if (useTexturing > 0)
-   	gl_FragColor = texture2D(tex0, uvVarying) * vColor;
-   else
-   	gl_FragColor = vColor;
+    gl_FragColor = texture2D(tex0, uvVarying) * vColor;
 }
+
