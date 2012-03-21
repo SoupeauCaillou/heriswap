d15a93f40dbf16eea3fb1c71f8bbecacbd67819b
Pierre-Eric Pelloux-Prayer <pelloux@gmail.com>
2012-03-21 21:43:15 +0100

    Add missing file

---
+++ sources/DepthLayer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define DL_Sky 0.01
+#define DL_Cloud 0.1
+#define DL_Background 0.15
+#define DL_CombinationMark 0.2
+#define DL_Cell 0.3
+#define DL_Hud 0.35
+#define DL_PauseUI 0.4
+#define DL_Score 0.45
+#define DL_MainMenuUI 0.5
+#define DL_Logo 0.55
+#define DL_Benchmark 0.6
+#define DL_Fading 0.65
+
+
