a7201de080b087055f1325bc58c0cbda46a26d48
Gautier Pelloux-Prayer <gautier@damsy.net>
2012-05-12 21:57:59 +0200

    [wip]modif algo musique

--- sources/Sound.cpp
+++ sources/Sound.cpp
@@ -5,44 +5,103 @@
 #include "Sound.h"
 #include <sstream>
 
-std::vector<std::string> newMusics() {
+std::vector<std::string> newMusics(std::vector<std::string> &olds) {
 	static std::vector<char> ppal;
 	if (ppal.size()==0) {
 		ppal.push_back('A');
 		ppal.push_back('B');
 	}
-
-	int count = MathUtil::RandomInt(4) + 1;
-	std::vector<char> l; // all songs id
 	std::vector<std::string> res;
+	
+	int a = olds.size();
+	int count = MathUtil::RandomInt(4);
+	if (a) 
+		count = MathUtil::RandomIntInRange(MathUtil::Max(1,a-1), a+1);
 
-	char c = ppal[MathUtil::RandomInt(ppal.size())]; // letter from main music
-	if (count==1) c='A';
-
-	std::stringstream s;
-	s<<"audio/"<<c<<".ogg";
-	l.push_back(c);
-	res.push_back(s.str());
-
-	for (int i=1; i<count; i++) {
-		do {
-			c = MathUtil::RandomInt(4)+'A';
-		} while (std::find(l.begin(), l.end(), c) != l.end());
-		l.push_back(c);
-		std::stringstream s;
-		s << "audio/" << c << ".ogg";
-		res.push_back(s.str());
+	if (count == 1) {
+		res.push_back("audio/A.ogg");
+		return res;
 	}
-	//std::cout <<"starting " << count <<" musics : ";
+	
+	char c;
+	std::vector<char> l; // all songs id
+	for (int i=0; i<olds.size(); i++) {
+		l.push_back(olds[i].size()-4);
+		LOGI("%c", olds[i].size()-4);
+	}
+	//on copie l'ancien dans le nouveau et on va modifier seulement un element
+	res = olds;
+	
+	//si on a gagné une piste(ou plusieurs si c'est la 1ere fois), on rajoute une lettre(s)
+	if (count > olds.size()) {
+		for (int i=0;i<count;i++) {
+			do {
+				c = MathUtil::RandomInt(4)+'A';
+			} while (std::find(l.begin(), l.end(), c) != l.end());
+			std::stringstream s;
+			s << "audio/" << c << ".ogg";
+			res.push_back(s.str());
+		}
+	//sinon si on en a perdu une, on supprime une qui n'est pas principale
+	} else if (count < olds.size()) {
+		int main = 0;
+		if (std::find(l.begin(), l.end(), 'A') != l.end())
+			main++;
+		if (std::find(l.begin(), l.end(), 'B') != l.end())
+			main++;
+		
+		int i = -1;
+		//si on a 2 pistes ppales, on se permet d'en supprimer une des deux
+		if (main==2) {
+			i =	MathUtil::RandomInt(count);
+		//sinon on tire une piste aléatoire secondaire
+		} else {
+			do {
+				i = MathUtil::RandomInt(count);
+			} while (res[i]=="audio/A.ogg"|| res[i]=="audio/B.ogg");
+		}
+		//puis on la supprime du resultat
+		res.erase(res.begin()+count);
+	//si on en a le même nombre, il faut en remplacer une
+	} else {
+		int main = 0;
+		if (std::find(l.begin(), l.end(), 'A') != l.end())
+			main++;
+		if (std::find(l.begin(), l.end(), 'B') != l.end())
+			main++;
+		
+		int i = MathUtil::RandomInt(res.size());		
+		//si on a 1 ppale et qu'on tire une ppale, faut faire ppal->ppal
+		if (main==1) {
+			res[i] = (res[i]=="audio/A.ogg")? "audio/B.ogg" : "audio/A.ogg";
+		//sinon on peut faire ppal->secondaire
+		} else {
+			do {
+				c = MathUtil::RandomInt(4)+'A';
+			} while (std::find(l.begin(), l.end(), c) != l.end());
+			std::stringstream s;
+			s << "audio/" << c << ".ogg";
+			res.push_back(s.str());
+		}
+
+	}
+	std::cout <<"starting " << count <<" musics : ";
+	for (int i=0; i<res.size()-1; i++) std::cout << res[i] <<", ";
+	std::cout << res[res.size()-1] << std::endl;
 	return res;
 }
 
 bool updateMusic(Canal* canal, Canal* canalStress1, Canal* canalStress2, float percentDone, float dt) {
+	//conserve olds musics
+	static std::vector<std::string> current;
 	bool end, endv[4];
-	for (int i=0; i<4; i++) endv[i] = canal[i].update(dt);
+	for (int i=0; i<4; i++)
+		endv[i] = canal[i].update(dt);
 	end=endv[0] && endv[1] && endv[2] && endv[3];
+	//if all musics ended, generate new ones
 	if (end) {
-		std::vector<std::string> nouv = newMusics();
+		std::vector<std::string> nouv = newMusics(current);
+		current = nouv;
 		for (int i=0; i<4; i++) {
 			if (!nouv.empty()) {
 				canal[i].name=nouv[0];
@@ -52,6 +111,7 @@ bool updateMusic(Canal* canal, Canal* canalStress1, Canal* canalStress2, float p
 			}
 		}
 	}
+	
 	float pos = 0;
 	int count = 0;
 	for (int i=0; i<4; i++) {
