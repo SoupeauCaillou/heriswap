9dada23cdb341d83053b023d75748356e84feb13
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@linphone.org>
2012-05-24 21:50:45 +0200

    NEw layout to reuse previous name

--- sources/modes/NormalModeManager.cpp
+++ sources/modes/NormalModeManager.cpp
@@ -78,6 +78,9 @@ float NormalGameModeManager::GameProgressPercent() {
 
 void NormalGameModeManager::UiUpdate(float dt) {
 	ADSR(stressTrack)->active = (time > 37.5);
+    if (ADSR(stressTrack)->active) {
+        ADSR(stressTrack)->sustainValue = MathUtil::Min(1 / (time - 37.5), 1.0);
+    }
 
 	//Score
 	{
