56d6235cd733886dc71ebc8a99336bd24047fdf4
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@linphone.org>
2012-05-25 18:19:49 +0200

    Fix level transition if sound is muted

--- sources/states/LevelStateManager.cpp
+++ sources/states/LevelStateManager.cpp
@@ -166,14 +166,14 @@ GameState LevelStateManager::Update(float dt) {
 		for (int i=0; i<modeMgr->branchLeaves.size(); i++) {
 			TRANSFORM(modeMgr->branchLeaves[i].e)->size = Game::CellSize(8) * Game::CellContentScale() * MathUtil::Min((duration-6) / 4.f, 1.f);
 			if (duration < 10) 
-				RENDERING(modeMgr->branchLeaves[i].e)->desaturate = false;
-			else 
+				RENDERING(modeMgr->branchLeaves[i].e)->desaturate = true;
+			else
 				RENDERING(modeMgr->branchLeaves[i].e)->desaturate = false;
 		}
 	}
 	
 	
-	if (MUSIC(eBigLevel)->music == InvalidMusicRef || duration > 10) {
+	if (duration > 10) {
 		return Spawn;
 	}
 
