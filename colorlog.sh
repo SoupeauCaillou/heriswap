60d4d94ef4d4e05e931c53d518da929f32d074de
Gautier Pelloux-Prayer <gpelloux@gmail.com>
2012-05-28 00:33:29 +0200

    colorlog modifs

--- colorlog.sh
+++ colorlog.sh
@@ -2,12 +2,19 @@
 
 while read LINE; do
 	w=`echo $LINE | grep WARN`
-	e=`echo $LINE | grep ERRO
-	`
-	if [ -n "$w" ]; then
+	e=`echo $LINE | grep ERRO`
+	i=`echo $LINE | grep INFO`
+
+	#local colors have priority
+	if [ -n "`echo $LINE | grep SuccessAPI::successCompleted`" ]; then
+		echo -e "\033[1m\E[91m$LINE\033[0m"; tput sgr0
+	#end local
+	elif [ -n "$w" ]; then
 		echo -e "\033[1m\E[33m$LINE\033[0m"; tput sgr0
 	elif [ -n "$e" ]; then
 		echo -e "\033[1m\E[31m$LINE\033[0m"; tput sgr0
+	elif [ -n "$i" ]; then
+		echo -e "\033[0m\E[36m$LINE\033[0m"; tput sgr0
 	else
 		echo -e "$LINE"
 	fi
