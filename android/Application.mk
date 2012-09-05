9d82526a75c3738aa055fdb46921c18e52b24aa7
Gautier Pelloux-Prayer <gpelloux@gmail.com>
2012-09-05 11:56:28 +0200

    rebut armeabi by default for ndk build

--- android/Application.mk
+++ android/Application.mk
@@ -3,6 +3,6 @@ APP_PROJECT_PATH := $(call my-dir)/../
 APP_BUILD_SCRIPT := $(call my-dir)/Android.mk
 APP_MODULES = sac heriswap libpng libtremor
 APP_OPTIM := release
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a armeabi
 APP_STL := stlport_static
 
