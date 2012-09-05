6df170e2f9e17da02738b9b00a86be9c8af5782e
Gautier Pelloux-Prayer <gpelloux@gmail.com>
2012-09-05 14:37:08 +0200

    Elite level is reset to 10

--- sources/states/LevelStateManager.cpp
+++ sources/states/LevelStateManager.cpp
@@ -213,7 +213,7 @@ GameState LevelStateManager::Update(float dt) {
 
 	//level animation ended - back to game
 	if (levelState == BigScoreMoving && duration > 10) {
-		if (currentLevel == 2 && theGridSystem.sizeToDifficulty() != DifficultyHard) {
+		if (currentLevel == 10 && theGridSystem.sizeToDifficulty() != DifficultyHard) {
 			return ElitePopup;
 		}
 		return Spawn;
