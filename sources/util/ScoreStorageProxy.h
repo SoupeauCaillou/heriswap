df0c419f322316802c271483e6c7b9f34b63fe27
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-04-25 20:07:20 +0200

    dont push new element by default

--- sources/util/ScoreStorageProxy.h
+++ sources/util/ScoreStorageProxy.h
@@ -45,5 +45,5 @@ class ScoreStorageProxy : public StorageProxy<Score> {
 
         std::string getValue(const std::string& columnName);
 
-        void setValue(const std::string& columnName, const std::string& value, bool pushNewElement = true);
+        void setValue(const std::string& columnName, const std::string& value, bool pushNewElement = false);
 };
