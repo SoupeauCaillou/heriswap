c938f9631d39dbed56fe4032764e9b0735923638
Jordane Pelloux-Prayer <jordane@damsy.net>
2013-06-20 22:31:55 +0200

    Add entity file of UI

--- sources/modes/InGameUiHelper.cpp
+++ sources/modes/InGameUiHelper.cpp
@@ -1,20 +1,20 @@
 /*
-	This file is part of Heriswap.
+    This file is part of Heriswap.
 
-	@author Soupe au Caillou - Pierre-Eric Pelloux-Prayer
-	@author Soupe au Caillou - Gautier Pelloux-Prayer
+    @author Soupe au Caillou - Pierre-Eric Pelloux-Prayer
+    @author Soupe au Caillou - Gautier Pelloux-Prayer
 
-	Heriswap is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, version 3.
+    Heriswap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
 
-	Heriswap is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    Heriswap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with Heriswap.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with Heriswap.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "InGameUiHelper.h"
 
@@ -36,78 +36,52 @@ InGameUiHelper::InGameUiHelper() : built(false) {
 }
 
 void InGameUiHelper::build() {
-	if (built)
-		return;
+    if (built)
+        return;
 
-	smallLevel = theEntityManager.CreateEntity("smallLevel");
-	ADD_COMPONENT(smallLevel, Transformation);
-	TRANSFORM(smallLevel)->position = glm::vec2((float)PlacementHelper::GimpXToScreen(680),
-												(float)PlacementHelper::GimpYToScreen(188));
-	TRANSFORM(smallLevel)->z = DL_Score;
-	ADD_COMPONENT(smallLevel, TextRendering);
-	TEXT_RENDERING(smallLevel)->color = Color(1, 1, 1);
-	TEXT_RENDERING(smallLevel)->fontName = "gdtypo";
-	TEXT_RENDERING(smallLevel)->charHeight = PlacementHelper::GimpHeightToScreen(232-150);
-	TEXT_RENDERING(smallLevel)->positioning = TextRenderingComponent::CENTER;
-	TEXT_RENDERING(smallLevel)->flags |= TextRenderingComponent::IsANumberBit;
+    smallLevel = theEntityManager.CreateEntity("smallLevel",
+        EntityType::Persistent, theEntityManager.entityTemplateLibrary.load("general/smallLevel"));
 
-	pauseButton = theEntityManager.CreateEntity("pauseButton");
-	ADD_COMPONENT(pauseButton, Transformation);
-    TRANSFORM(pauseButton)->size = glm::vec2(PlacementHelper::GimpWidthToScreen(100), PlacementHelper::GimpHeightToScreen(95));
-    // TransformationSystem::setPosition(TRANSFORM(pauseButton), glm::vec2(-PlacementHelper::GimpWidthToScreen(354), PlacementHelper::GimpYToScreen(1215)), TransformationSystem::W);
-    TRANSFORM(pauseButton)->z = DL_Score;
-	ADD_COMPONENT(pauseButton, Rendering);
-	RENDERING(pauseButton)->texture = theRenderingSystem.loadTextureFile("pause");
-    ADD_COMPONENT(pauseButton, Sound);
-	ADD_COMPONENT(pauseButton, Button);
-    BUTTON(pauseButton)->overSize = 1.6;
-	BUTTON(pauseButton)->enabled = false;
+    pauseButton = theEntityManager.CreateEntity("pauseButton",
+        EntityType::Persistent, theEntityManager.entityTemplateLibrary.load("general/pauseButton"));
 
-	scoreProgress = theEntityManager.CreateEntity("scoreProgress");
-	ADD_COMPONENT(scoreProgress, Transformation);
-	TRANSFORM(scoreProgress)->z = DL_Score;
-	TRANSFORM(scoreProgress)->position = glm::vec2(0, PlacementHelper::GimpYToScreen(1215));
-	ADD_COMPONENT(scoreProgress, TextRendering);
-	TEXT_RENDERING(scoreProgress)->color = Color(3.0/255, 99.0/255, 71.0/255);
-	TEXT_RENDERING(scoreProgress)->fontName = "typo";
-	TEXT_RENDERING(scoreProgress)->charHeight = PlacementHelper::GimpHeightToScreen(47);
-	TEXT_RENDERING(scoreProgress)->positioning = TextRenderingComponent::CENTER;
-	TEXT_RENDERING(scoreProgress)->flags |= TextRenderingComponent::IsANumberBit;
+    scoreProgress = theEntityManager.CreateEntity("scoreProgress",
+        EntityType::Persistent, theEntityManager.entityTemplateLibrary.load("general/scoreProgress"));
 
-	built = true;
+    built = true;
 }
 
 void InGameUiHelper::show() {
-	if (!built)
-		return;
+    if (!built)
+        return;
 
-	TEXT_RENDERING(smallLevel)->show = true;
-	RENDERING(pauseButton)->show = true;
-	TEXT_RENDERING(scoreProgress)->show = true;
-	BUTTON(pauseButton)->enabled=true;
+    TEXT_RENDERING(smallLevel)->show = true;
+    RENDERING(pauseButton)->show = true;
+    TEXT_RENDERING(scoreProgress)->show = true;
+    BUTTON(pauseButton)->enabled=true;
 }
 
 void InGameUiHelper::update(float) {
-	// handle button
-	if (BUTTON(pauseButton)->clicked) {
-		SOUND(pauseButton)->sound = theSoundSystem.loadSoundFile("audio/son_menu.ogg");
-		game->togglePause(true);
-	}
+    // handle button
+    if (BUTTON(pauseButton)->clicked) {
+        SOUND(pauseButton)->sound = theSoundSystem.loadSoundFile("audio/son_menu.ogg");
+        game->togglePause(true);
+    }
 }
 
 void InGameUiHelper::hide() {
-	if (!built)
-		return;
-	TEXT_RENDERING(smallLevel)->show = false;
-	RENDERING(pauseButton)->show = false;
-	BUTTON(pauseButton)->enabled = false;
-	TEXT_RENDERING(scoreProgress)->show = false;
+    if (!built)
+        return;
+    TEXT_RENDERING(smallLevel)->show = false;
+    RENDERING(pauseButton)->show = false;
+    BUTTON(pauseButton)->enabled = false;
+    TEXT_RENDERING(scoreProgress)->show = false;
 }
 
 void InGameUiHelper::destroy() {
-	if (!built)
-		return;
-	theEntityManager.DeleteEntity(smallLevel);
-	theEntityManager.DeleteEntity(pauseButton);
-	theEntityManager.DeleteEntity(scoreProgress);
+    if (!built)
+        return;
+    theEntityManager.DeleteEntity(smallLevel);
+    theEntityManager.DeleteEntity(pauseButton);
+    theEntityManager.DeleteEntity(scoreProgress);
 }
