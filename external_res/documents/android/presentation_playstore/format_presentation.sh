13b7ba5764256747c8b6b70bc6a1980645178de6
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-10-20 22:11:53 +0200

    Use -n option (no new line by default)

--- external_res/documents/android/presentation_playstore/format_presentation.sh
+++ external_res/documents/android/presentation_playstore/format_presentation.sh
@@ -13,13 +13,25 @@ fi
 
 declare -A semantic
 #add there your specific presentation format
-semantic[1]="\n"
+semantic[1]="\n\n"
+semantic[2]="\n"
+semantic[3]="\n"
+semantic[4]="\n"
+semantic[5]="\n"
+semantic[6]="\n"
+semantic[7]="\n"
+semantic[8]="\n"
+semantic[9]="\n"
+semantic[10]="\n"
+semantic[11]="\n"
+semantic[12]="\n"
+semantic[13]="\n"
 
 
 function parse_file {
     c=1
     while read line; do
-        echo -e "$line ${semantic[$c]}"
+        echo -ne "$line ${semantic[$c]}"
         c=$(($c + 1))
     done < $file
 }
