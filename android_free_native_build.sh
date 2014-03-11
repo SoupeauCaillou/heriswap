a9d1ccb27fcc2b952dc3f3592a671c08abd3e88f
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2014-03-11 16:33:17 +0100

    Exit code != 0 if error

--- android_free_native_build.sh
+++ android_free_native_build.sh
@@ -9,14 +9,21 @@ cd $(dirname $0)
 # Remove Google Play plugin from APK
 sed -i '/SacGooglePlay/d' project.properties
 
-# Generate arm & x86 APK
 
+cancel_changes() {
+	git checkout project.properties res/values/plugins.xml AndroidManifest.xml
+}
+
+# Generate arm & x86 APK
 if ! bash sac/tools/build/build-all.sh --target android -x86 -release n -c --c "-DSAC_RESTRICTIVE_PLUGINS=0"; then
     echo "Error when building x86 version"
+    cancel_changes
+    exit 1
 elif ! bash sac/tools/build/build-all.sh --target android -arm -release n -c --c "-DSAC_RESTRICTIVE_PLUGINS=0"; then
     echo "Error when building ARM version"
+    cancel_changes
+    exit 2
 fi        
 
 # Then cancel changes
-git checkout project.properties res/values/plugins.xml
-git checkout AndroidManifest.xml
+cancel_changes
